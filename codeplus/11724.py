75096f46a8424ea3b248ed1612db4ea47b8c069c
sulungsLim <zzz90zzz@likelion.org>
2020-04-07 03:07:22 +0900

    Create Baekjoon folder

---
+++ codeplus/11724.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 03:02:55 2020
+
+@author: USER
+"""
+
+a,b=map(int,input().split())
+ls=[]
+
+for i in range(b):
+    ls.append(input().split())
+    print(ls)
+    hi=[ls[0]]
+c=0
+for sum in range(b):
+    for k in range(2):    
+        if ls[sum][k] in hi[c]:
+            hi[c] += ls[sum]
+            hi[c] = list(set(hi[c]))
+            print(hi)
+            pass
+            
+        elif ls[sum][k] not in hi[c]:
+            hi.append(ls[sum])
+            c += 1
+            hi[c] = list(set(hi[c]))
+            print(hi)
+            break
+        
+
+print(len(hi))
